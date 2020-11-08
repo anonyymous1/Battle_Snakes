d7ffefb9c81537c667f66221806cb89600aa742d
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-11-07 23:53:55 -0500

    [Update]Add styling and function buttons

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Battle_Snakes
-This is my first project, Battle Snakes
+# Battle Snakes!
+Project One: Creating my game!
+
+## About Battle Snakes & How to play
+
+I wanted to create a game that could be played by two people on the same keyboard. Using the arrow functions you can move the snake Up, Down, Left and Right. Try to collect as many items as you can but be careful not to hit the walls or yourself. 
+
+
+## Challenges that came my way.
+
+Styling the game came easy. Coding on the other hand wrecked my brain. The movemnt of the snake came easy and random functions to spawn the items came quick. The issues I had mainly revolved around the movement of the snakesbody and body functions
+
+### Peak at my Code
+
+```html
+
+```
+
+```css
+
+```
+
+```javascript
+
+```
+
+### Things i want to add in the future
+
+My goal is to get the game at least working for one player. But in the future i plan on adding the following:
+
+- Random items that have other effects like double growth or extra speed.
+- Increase speed with each item eaten.
+- Implement obstacles that are either fixed on the stage or moving so you can dodge.
+- Ability to change the wallpaper in the home function.
+- Better graphics for the snake or use images 
--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 //------------------------------------------------------------GAME CANVAS STARTERS
-
 const game = document.querySelector('#game');
 const computedStyle = getComputedStyle(game);
-
+const ctx = game.getContext('2d');
 const height = computedStyle.height;
 const width = computedStyle.width;
 game.height = parseInt(height); 
 game.width = parseInt(width);
 
-const ctx = game.getContext('2d');
-
-
+let score = 0;
+let fps = 120;
 let snakeP1Array = [];
 
-//----------------------------------------------------------------EVENT LISTENERS
-
-
-//----------------------------------------------------------------SNAKE CREATOR
+//----------------------------------------------------------------SNAKE & FOOD CONSTRUCTORS
 
 function Food (x, y, width, height, color) {
     this.x = x;
@@ -31,6 +26,7 @@ function Food (x, y, width, height, color) {
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
+
 function newApple() {
     random_x = Math.floor(Math.random() * (game.height - 15)); //need to take the height number
     random_y = Math.floor(Math.random() * (game.width - 15)); // need to take the width number   
@@ -52,10 +48,11 @@ class Snake {
     }   
 }
 
-
+//----------------------------------------------------------------CALLING FOR SNAKE & FOOD
+let apple = new Food(150, 100, 15, 15, 'red');
 const snakeP1 = new Snake(100, 100, 'darkgreen', 30, 30);
 snakeP1Array.push(snakeP1);
-const snakeP2 = new Snake(400, 400, 'purple', 30, 30);
+
 
 
 //---------------------------------------------------------------MOVEMENT FOR PLAYERS
@@ -72,7 +69,137 @@ document.addEventListener('keyup', function(evt){
     }
 })
 
+//---------------------------------------------------------------------------FUNCTIONS
+
+function playerUp(snakeArray){
+    for (let i = 0; i < snakeArray.length; i++) {
+        let snakeBody = snakeArray[i];
+        snakeBody.y -= 1;
+        snakeBody.velX = 0;
+        snakeBody.velY = -1;
+    }
+}
+
+function playerDown(snakeArray){
+    for (let i = 0; i < snakeArray.length; i++) {
+        let snakeBody = snakeArray[i];
+        snakeBody.y += 1;
+        snakeBody.velX = 0;
+        snakeBody.velY = 1;    
+    }
+}
+
+function playerLeft(snakeArray){
+    for (let i = 0; i < snakeArray.length; i++) {
+        let snakeBody = snakeArray[i];
+        snakeBody.x -= 1;
+        snakeBody.velX = -1;
+        snakeBody.velY = 0;
+    }
+}
+
+function playerRight(snakeArray){
+    for (let i = 0; i < snakeArray.length; i++) {
+        let snakeBody = snakeArray[i];
+        snakeBody.x += 1;
+        snakeBody.velX = 1;
+        snakeBody.velY = 0;
+    }
+}
+
+//TA QUESTIONS
+function outOfBounds(player) {
+    if (player.x + 30 > game.width ||
+        player.x <= -1 ||
+        player.y + 30  > game.height ||
+        player.y <= -1) {
+        const a = document.getElementById("lose");
+        a.style.display = "block";
+        clearInterval(frames());
+
+    }
+}
+    
+function appleEaten() {
+    if (snakeP1.x + snakeP1.width > apple.x &&
+        snakeP1.x < apple.x + apple.width &&
+        snakeP1.y + snakeP1.height > apple.y &&
+        snakeP1.y < apple.y + apple.height ) { 
+        score++;
+        apple.alive = false;
+        addTail();
+        newApple(apple);
+        apple = new Food(random_x, random_y, 15, 15, 'white');
+    }
+}  
+
+//TA QUESTIONS
+function addTail() {
+    let snakeTail = snakeP1Array[snakeP1Array.length - 1];
+    let snakeBody = new Snake(snakeTail.x-30, snakeTail.y, 'lightgreen', 30, 30);
+    snakeBody.velX = snakeTail.velX;
+    snakeBody.velY = snakeTail.velY;
+    snakeP1Array.push(snakeBody);
+}
+
+function playGame() {
+    const a = document.getElementById("container");
+    a.style.display = "block";
+    const b = document.getElementById("reset");
+    b.style.display = "inline";
+    const c = document.getElementById("play");
+    c.style.display = "none";
+    const d = document.getElementById("directions");
+    d.style.display = "none";
+    const e = document.getElementById("scoreTitle");
+    e.style.display = "inline";
+    const f = document.getElementById("score");
+    f.style.display = "inline";
+    const g = document.getElementById("logo");
+    g.style.marginTop = "0px";
+    const h = document.getElementById("logo");
+    h.style.width = "300px";
+
+}
+
+function resetGame() {
+    window.location.reload();
+}
+
+function frames(){
+    ctx.clearRect(0, 0, game.width, game.height)
+
+    for (let i = 0; i < snakeP1Array.length; i++) {
+        let snakeBody = snakeP1Array[i];
+        snakeBody.render();
+        snakeBody.x += snakeBody.velX;
+        snakeBody.y += snakeBody.velY;
+    }
+    
+    document.getElementById('score').textContent = " " + score;
+    
+    snakeP1.render();
+    outOfBounds(snakeP1)
+    apple.render();
+    appleEaten();
+    
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    setInterval(frames,1000 / fps)
+});
+
+
+
+
+
+
+
+
+//UNUSED
 //Player Two
+// const snakeP2 = new Snake(400, 400, 'purple', 30, 30);
 // document.addEventListener('keyup', function(evt){
 //     if (evt.key === "ArrowUp") {
 //         playerUp(snakeP2); 
@@ -85,134 +212,6 @@ document.addEventListener('keyup', function(evt){
 //     }
 // })
 
-//---------------------------------------------------------------------------FUNCTION
-
-
-function playerUp(snakeArray){
-    for (let i = 0; i < snakeArray.length; i++) {
-        let snakeBody = snakeArray[i];
-        snakeBody.y -= 1;
-        snakeBody.velX = 0;
-        snakeBody.velY = -1;
-        // snakeArray.pop(snakeArray.length[snakeP1Array.length - 1])
-        // snakeArray.push(snakeBody);
-    }
-}
-
-function playerDown(snakeArray){
-    for (let i = 0; i < snakeArray.length; i++) {
-        let snakeBody = snakeArray[i];
-        snakeBody.y += 1;
-        snakeBody.velX = 0;
-        snakeBody.velY = 1;
-          
-    }
-    // player.y += 1;
-    // player.velX = 0;
-    // player.velY = 1; 
-}
-
-function playerLeft(snakeArray){
-    for (let i = 0; i < snakeArray.length; i++) {
-        let snakeBody = snakeArray[i];
-        snakeBody.x -= 1;
-        snakeBody.velX = -1;
-        snakeBody.velY = 0;
-    }
-    // player.x -= 1;
-    // player.velX = -1;
-    // player.velY = 0;  
-}
-function playerRight(snakeArray){
-    for (let i = 0; i < snakeArray.length; i++) {
-        let snakeBody = snakeArray[i];
-        snakeBody.x += 1;
-        snakeBody.velX = 1;
-        snakeBody.velY = 0;
-    }
-    // player.x += 1;
-    // player.velX = 1;
-    // player.velY = 0; 
-}
-
-
-function outOfBounds(player) {
-    if (player.x > game.width) {
-        player.x = 0;
-    }
-    if (player.x <= -1) {
-        player.x = game.width;
-    }
-    if (player.y > game.height) {
-        player.y = 0;
-    }
-    if (player.y <= -1) {
-        player.y = game.height;
-    }
-}
-function appleEaten() {
-    if (snakeP1.x + snakeP1.width > apple.x &&
-        snakeP1.x < apple.x + apple.width &&
-        snakeP1.y + snakeP1.height > apple.y &&
-        snakeP1.y < apple.y + apple.height ) { 
-        
-        apple.alive = false;
-        addTail();
-        newApple(apple);
-        apple = new Food(random_x, random_y, 15, 15, 'white');
-    }
-}  
-
-let apple = new Food(150, 100, 15, 15, 'red');
-
-
-function addTail() {
-    let snakeTail = snakeP1Array[snakeP1Array.length - 1];
-    let snakeBody = new Snake(snakeTail.x-30, snakeTail.y, 'lightgreen', 30, 30);
-    snakeBody.velX = snakeTail.velX;
-    snakeBody.velY = snakeTail.velY;
-    snakeP1Array.push(snakeBody);
-}
-
-
-
-function frames(){
-    ctx.clearRect(0, 0, game.width, game.height)
-
-    for (let i = 0; i < snakeP1Array.length; i++) {
-        let snakeBody = snakeP1Array[i];
-        // console.log(snakeBody);
-        snakeBody.render();
-        snakeBody.x += snakeBody.velX;
-        snakeBody.y += snakeBody.velY;
-    }
-    
-
-    // for (let i = (snakeP1Array.length - 1); i > 0; i--){ // i is always going to start at 0
-    //     snakeP1Array[i].x = snakeP1Array[i - 1].x; //put its in different place in the array 
-    //     snakeP1Array[i].y = snakeP1Array[i - 1].y; // minus    
-     
-    //   } 
-
-
-
-    snakeP1.render();
-    
-    
-    outOfBounds(snakeP1)
-    apple.render();
-    // snakeP1.x += snakeP1.velX;
-    // snakeP1.y += snakeP1.velY;
-    
-    appleEaten();
-    // Player Two
-    // snakeP2.render()
-    // outOfBounds(snakeP2)
-    // snakeP2.x += snakeP2.velX;
-    // snakeP2.y += snakeP2.velY;  
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-    let fps = 120;
-    setInterval(frames,1000 / fps)
-});
+// Player Two
+// snakeP2.render()
+// outOfBounds(snakeP2) 
